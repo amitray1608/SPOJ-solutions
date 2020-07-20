c7b4839c6c19161b657f548b74ba79dc4d603d9f
AMIT KUMAR <44608303+amitray1608@users.noreply.github.com>
2020-07-20 12:05:46 +0530

    All Pair Shortest Path

--- TRVCOST.cpp
+++ TRVCOST.cpp
@@ -8,56 +8,47 @@ const int MOD = 1e9 + 7;
 #define endl '\n'
 #define deb(x) cout << #x << " = " << x << endl;
 
-const int INF = 1e9;
-typedef pair<int, int> pri;
-vector<vector<pri>>adj;
-vector<int>dis;
-
-
-void dijk(int s) {
-  priority_queue<pri, vector<pri>, greater<pri>>q;
-  dis[s] = 0;
-  q.push({0, s});
-  while(!q.empty()) {
-    auto x = q.top();
-    q.pop();
-    int u = x.second;
-    if(x.first == INF) continue;
-    for(auto i : adj[u]) {
-      int v = i.first;
-      int c = i.second;
-      if(dis[v] > dis[u]+c) {
-        dis[v] = dis[u]+c;
-        q.push({dis[v], v});
-      }
-    }
-  }
-}
+const ll INF = 1e9;
+vector<vector<ll>>dis;
 
 void solve(){
   int n = 501;
   int m;
   cin >> m;
-  adj.resize(n);
-  dis.resize(n, INF);
+  dis.resize(n, vector<ll>(n, INF));
   for(int i = 0; i < m; i++) {
-    int u, v, c;
+    int u, v;
+    ll c;
     cin >> u >> v >> c;
-    adj[u].push_back({v, c});
-    adj[v].push_back({u, c});
+
+    dis[u][v] = min(dis[u][v], c);
+    dis[v][u] = min(dis[v][u], c);
   }
   int u;
   cin >> u;
-  dijk(u);
+  for(int i = 0; i < n; i++) {
+    dis[i][i] = 0;
+  }
+  //Floyd
+  for(int k = 0; k < n; k++) {
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
+        dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j]);
+      }
+    }
+  }
+
   int q;
   cin >> q;
   while(q--) {
     int d;
     cin >> d;
-    if(dis[d] >= INF)
+    if(u == d)
+      cout << 0 << endl;
+    else if(dis[u][d] >= INF or d > 500)
       cout << "NO PATH" << endl;
-    else
-      cout << dis[d] << endl;
+    else 
+      cout << dis[u][d] << endl;
   }
 }
 
